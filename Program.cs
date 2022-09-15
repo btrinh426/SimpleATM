a1c8012778267c2681eebb88431ceb02bd211715
btrinh426 <btrinh426@yahoo.com>
2022-09-15 13:10:28 -0700

    finished simple atm app

--- Program.cs
+++ Program.cs
@@ -87,8 +87,9 @@ namespace ConsoleApp
             {
                 Console.WriteLine("How much $$ would you like to deposit: ");
                 int deposit = int.Parse(Console.ReadLine());
-                currentUser.setBalance(deposit);
-                Console.WriteLine("Thank you for your $$. Your new balance is: " + currentUser.getBalance());
+                int newBalance = currentUser.getBalance() + deposit;
+                // currentuser.setbalance(deposit);
+                Console.WriteLine("Thank you for your $$. Your new balance is: " + newBalance);
             }
 
             void withdraw(cardHolder currentUser)
@@ -102,6 +103,7 @@ namespace ConsoleApp
                 {
                     int newBalance = currentUser.getBalance() - withdraw;
                     currentUser.setBalance(newBalance);
+                    Console.WriteLine("Your new balance is: " + newBalance);
                     Console.WriteLine("You're good to go! Thank you :)");
                 }
             }
@@ -142,6 +144,47 @@ namespace ConsoleApp
                 }
             }
 
+            Console.WriteLine("Please enter your pin");
+
+            int userPin = 0;
+
+            while (true)
+            {
+                try
+                {
+                    userPin = int.Parse(Console.ReadLine());
+                    if (currentUser.getPin() == userPin) { break; }
+                    else
+                    {
+                        Console.WriteLine("Incorrect pin. Please try again");
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Incorrect pin. Please try again");
+                }
+            }
+
+            Console.WriteLine("Welcome" + currentUser.getFirstName() + ":)");
+            int option = 0;
+            do
+            {
+                printOptions();
+                try
+                {
+                    option = int.Parse(Console.ReadLine());
+                }
+                catch
+                {
+
+                }
+                if(option == 1) { deposit(currentUser); } 
+                else if (option == 2) { withdraw(currentUser); }
+                else if (option == 3) { balance(currentUser); } 
+                else if (option == 4) { break; } 
+                else { option = 0; }
+            } while (option != 4);
+            Console.WriteLine("Thank you! Have a nice day :)");
         }
     }
 }
